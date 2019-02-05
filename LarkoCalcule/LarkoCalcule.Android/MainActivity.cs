d2c972f888885d81e88db4966e8118cd0c6dabb8
Vincent FILLON <vincent_fillon@hotmail.com>
2019-02-05 14:46:41 +0100

    Positionnement bouton effectué

--- LarkoCalcule/LarkoCalcule.Android/MainActivity.cs
+++ LarkoCalcule/LarkoCalcule.Android/MainActivity.cs
@@ -16,7 +16,6 @@ namespace LarkoCalcule.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
