2fb84c6519c9ccff45a996247d5cca07b59d4ae6
Vincent FILLON <vincent_fillon@hotmail.com>
2019-02-20 15:19:27 +0100

    Page Division terminée et fonctionnelle

--- LarkoCalcule/LarkoCalcule/Views/DividePage.xaml.cs
+++ LarkoCalcule/LarkoCalcule/Views/DividePage.xaml.cs
@@ -18,30 +18,99 @@ namespace LarkoCalcule.Views
         {
             InitializeComponent();
             jeuDivide = new Jeu();
-
+            InitPageDivision();
         }
+
+        private void InitPageDivision()
+        {
+            jeuDivide.GenererAleatoiresChiffres();
+            while (jeuDivide.Chiffre1 < jeuDivide.Chiffre2 || (jeuDivide.Chiffre1 % jeuDivide.Chiffre2 != 0))
+            {
+                jeuDivide.GenererAleatoiresChiffres();
+            }
+            txtChiffre1.Text = (jeuDivide.Chiffre1).ToString();
+            txtChiffre2.Text = (jeuDivide.Chiffre2).ToString();
+            jeuDivide.VraiResultat = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            jeuDivide.GenererAleatoiresChiffres();
+            jeuDivide.Fauxresultat1 = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            while (jeuDivide.Fauxresultat1 == jeuDivide.VraiResultat || jeuDivide.Chiffre1 < jeuDivide.Chiffre2 || jeuDivide.Chiffre1 % jeuDivide.Chiffre2 != 0)
+            {
+                jeuDivide.GenererAleatoiresChiffres();
+                jeuDivide.Fauxresultat1 = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            }
+            
+            jeuDivide.GenererAleatoiresChiffres();
+            jeuDivide.Fauxresultat2 = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            while (jeuDivide.Fauxresultat2 == jeuDivide.VraiResultat || jeuDivide.Fauxresultat2 == jeuDivide.Fauxresultat1 || jeuDivide.Chiffre1 < jeuDivide.Chiffre2 || jeuDivide.Chiffre1 % jeuDivide.Chiffre2 != 0)
+            {
+                jeuDivide.GenererAleatoiresChiffres();
+                jeuDivide.Fauxresultat2 = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            }
+            
+            jeuDivide.GenererAleatoiresChiffres();
+            jeuDivide.Fauxresultat3 = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            while (jeuDivide.Fauxresultat3 == jeuDivide.VraiResultat || jeuDivide.Fauxresultat3 == jeuDivide.Fauxresultat1 || jeuDivide.Fauxresultat3 == jeuDivide.Fauxresultat2 || jeuDivide.Chiffre1 < jeuDivide.Chiffre2 || jeuDivide.Chiffre1 % jeuDivide.Chiffre2 != 0)
+            {
+                jeuDivide.GenererAleatoiresChiffres();
+                jeuDivide.Fauxresultat3 = jeuDivide.Chiffre1 / jeuDivide.Chiffre2;
+            }
+           
+            jeuDivide.PositionnerLesResultatsAleatoirement();
+            btnResult1.Text = jeuDivide.Position1;
+            btnResult2.Text = jeuDivide.Position2;
+            btnResult3.Text = jeuDivide.Position3;
+            btnResult4.Text = jeuDivide.Position4;
+        }
+
         private void btnAutreCalculClicked(object sender, EventArgs e)
         {
-            ;
+            InitPageDivision();
         }
-                
-       
-        
+
+
         private void btnResult1Clicked(object sender, EventArgs e)
         {
-            ;
+            if ((jeuDivide.VraiResultat).ToString() == btnResult1.Text)
+            {
+                this.Navigation.PushAsync(new BravoPage());
+            }
+            else if ((jeuDivide.Fauxresultat1).ToString() == btnResult1.Text || (jeuDivide.Fauxresultat2).ToString() == btnResult1.Text || (jeuDivide.Fauxresultat3).ToString() == btnResult1.Text)
+            {
+                this.Navigation.PushAsync(new ErrorPage());
+            }
         }
         private void btnResult2Clicked(object sender, EventArgs e)
         {
-            ;
+            if ((jeuDivide.VraiResultat).ToString() == btnResult2.Text)
+            {
+                this.Navigation.PushAsync(new BravoPage());
+            }
+            else if ((jeuDivide.Fauxresultat1).ToString() == btnResult2.Text || (jeuDivide.Fauxresultat2).ToString() == btnResult2.Text || (jeuDivide.Fauxresultat3).ToString() == btnResult2.Text)
+            {
+                this.Navigation.PushAsync(new ErrorPage());
+            }
         }
         private void btnResult3Clicked(object sender, EventArgs e)
         {
-            ;
+            if ((jeuDivide.VraiResultat).ToString() == btnResult3.Text)
+            {
+                this.Navigation.PushAsync(new BravoPage());
+            }
+            else if ((jeuDivide.Fauxresultat1).ToString() == btnResult3.Text || (jeuDivide.Fauxresultat2).ToString() == btnResult3.Text || (jeuDivide.Fauxresultat3).ToString() == btnResult3.Text)
+            {
+                this.Navigation.PushAsync(new ErrorPage());
+            }
         }
         private void btnResult4Clicked(object sender, EventArgs e)
         {
-            ;
+            if ((jeuDivide.VraiResultat).ToString() == btnResult4.Text)
+            {
+                this.Navigation.PushAsync(new BravoPage());
+            }
+            else if ((jeuDivide.Fauxresultat1).ToString() == btnResult4.Text || (jeuDivide.Fauxresultat2).ToString() == btnResult4.Text || (jeuDivide.Fauxresultat3).ToString() == btnResult4.Text)
+            {
+                this.Navigation.PushAsync(new ErrorPage());
+            }
         }
     }
 }
